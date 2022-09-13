4be511d72e84e9244442784742f906a912b56643
helmavdl <helmavdl@users.noreply.github.com>
2022-09-13 14:01:57 +0000

    Update swagger-ui to v4.14.0

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "openapi-bgz.yml",
+    url: "openapi-bgz.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
