57fccc9dd695709189595d8abd82fe96f73a5fa8
Helma van der Linden <helma.van.der.linden@vzvz.nl>
2022-09-13 16:18:39 +0200

    FIX typo in filename

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "openapi-bgz.yaml",
+    url: "openapi-bgz.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
