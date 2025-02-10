de820fbc9572437de3868d52a29706be02bad851
Helma van der Linden <helma.van.der.linden@vzvz.nl>
2025-02-10 17:39:06 +0100

    MAINTENANCE update workflow to use current version of GitHub Action and add debug info to script

--- prepare-docs.sh
+++ prepare-docs.sh
@@ -12,6 +12,8 @@ for l in $( ls -1 "${ROOT_DIR}"/api/open*.yml ); do
     file=$( basename "${l}" )
 
     # create symlinks for each openapi file
+    echo creating symlink for "${file}"
+
     ln -sf "../api/${file}" "${ROOT_DIR}/docs/${file}"
 
     # create an html version for each openapi file
@@ -20,4 +22,6 @@ for l in $( ls -1 "${ROOT_DIR}"/api/open*.yml ); do
 
     sed -e "s|OPENAPI_YML_FILE|${url}|" "${ROOT_DIR}/TEMPLATE.html" \
     > "${ROOT_DIR}/docs/${file%%.*}.html"
+
+    ls -1 "${ROOT_DIR}/docs/${file%%.*}.html"
 done
